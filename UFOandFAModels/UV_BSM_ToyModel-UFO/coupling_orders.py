713ab0f8d73f0f5477152473c1d587e9fb651807
viniciusrocca <vinicius_rocca@hotmail.com>
2025-06-17 23:44:20 -0300

    Trying to get the UFO filefor the UV BSM Toy model

---
+++ UFOandFAModels/UV_BSM_ToyModel-UFO/coupling_orders.py
@@ -0,0 +1,28 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 14.1.0 for Linux x86 (64-bit) (July 16, 2024)
+# Date: Tue 17 Jun 2025 23:43:21
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP1 = CouplingOrder(name = 'NP1',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+NP2 = CouplingOrder(name = 'NP2',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+NP3 = CouplingOrder(name = 'NP3',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 3)
+
