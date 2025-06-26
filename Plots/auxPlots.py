464e98e2ecd85547cdb1d61113df9f1b890b84da
viniciusrocca <vinicius_rocca@hotmail.com>
2025-06-26 00:31:07 -0300

    Added event files. First version of plot files added.

---
+++ Plots/auxPlots.py
@@ -0,0 +1,120 @@
+
+import glob, pyslha,os
+import numpy as np
+import itertools
+from scipy.interpolate import  griddata
+from matplotlib import pyplot as plt
+import tempfile
+import pylhe
+import gzip
+
+
+
+def getLHEevents(fpath):
+    """
+    Reads a set of LHE files and returns a dictionary with the file labels as keys
+    and the PyLHE Events object as values.
+    """
+
+    # It is necessary to remove the < signs from the LHE files (in the generate line) before parsing with pylhe
+    fixedFile = tempfile.mkstemp(suffix='.lhe')
+    os.close(fixedFile[0])
+    fixedFile = fixedFile[1]
+    with  gzip.open(fpath,'rt') as f:
+        data = f.readlines()
+        with open(fixedFile,'w') as newF:
+            for l in data:
+                if 'generate' in l:
+                    continue
+                newF.write(l)
+    events = list(pylhe.read_lhe_with_attributes(fixedFile))
+    nevents = pylhe.read_num_events(fixedFile)
+    os.remove(fixedFile)
+    return nevents,events
+
+def getDistributions(filename):
+
+    nevents,events = getLHEevents(filename)
+    pT1 = []
+    pT2 = []
+    mTT = []
+    weights = []
+    for ev in events:
+        w = ev.eventinfo.weight/nevents
+        weights.append(w)
+        for ptc in ev.particles:
+            if abs(ptc.id) != 6: continue
+            if ptc.id == 6:
+                pA = np.array([ptc.px,ptc.py,ptc.pz,ptc.e])
+            else:
+                pB = np.array([ptc.px,ptc.py,ptc.pz,ptc.e])
+
+        pT1.append(max(np.linalg.norm(pA[0:3]),np.linalg.norm(pB[0:3])))
+        pT2.append(min(np.linalg.norm(pA[0:3]),np.linalg.norm(pB[0:3])))
+        mTT.append(np.sqrt((pA[-1]+pB[-1])**2-np.linalg.norm(pA[0:3]+pB[0:3])**2))
+    
+    dists = {'mTT' : mTT, 'pT1' : pT1, 'pT2' : pT2, 
+             'weights' : np.array(weights), 'nevents' : nevents}
+
+    return dists
+
+
+def getInfo(f,labelsDict=None):
+
+
+    if labelsDict is None:
+        labelsDict = {'Top-FormFactorsOneLoop-UFO' : '1-loop', 'Top-EFT-UFO' : 'EFT', 
+                      'Top-EFTphysical_simple-UFO' : 'EFT',
+                      'SMS-stop-UFO' : 'SM', 'SMS-stop-NLO_SMQCD-UFO' : 'SM',
+              'g g > t t~' : r'$g g \to \bar{t} t$', 'g g > t~ t' : r'$g g \to \bar{t} t$',
+              'q q > t t~' : r'$q q \to \bar{t} t$', 'q q > t~ t' : r'$q q \to \bar{t} t$',
+              'p p > t t~' : r'$p p \to \bar{t} t$', 'p p > t~ t' : r'$p p \to \bar{t} t$'
+             }
+    
+    banner = list(glob.glob(os.path.join(os.path.dirname(f),'*banner*')))[0]
+    with open(banner,'r') as bannerF:
+        bannerData = bannerF.read()
+    
+    # Get process data:
+    processData = bannerData.split('<MGProcCard>')[1].split('</MGProcCard>')[0]
+#     print(processData)
+    # Get model
+    model = processData.split('Begin MODEL')[1].split('End   MODEL')[0]
+    model = model.split('\n')[1].strip()
+    if model in labelsDict:
+        model = labelsDict[model]
+    # Get process
+    proc = processData.split('Begin PROCESS')[1].split('End PROCESS')[0]
+    proc = proc.split('\n')[1].split('#')[0].strip()
+    if proc in labelsDict:
+        proc = labelsDict[proc]
+    
+    # Get parameters data:
+    parsData = bannerData.split('<slha>')[1].split('</slha>')[0]
+    parsSLHA = pyslha.readSLHA(parsData)
+    
+    mT  = parsSLHA.blocks['MASS'][6]
+    if 5000002 in parsSLHA.blocks['MASS']:
+        mSDM = parsSLHA.blocks['MASS'][5000002]
+        mPsiT = parsSLHA.blocks['MASS'][5000012]        
+        yDM = list(parsSLHA.blocks['FRBLOCK'].values())[-1]
+    else:
+        mSDM = 0.0
+        mPsiT = 0.0
+        yDM = 0.0
+
+    if yDM == 0.0:
+        model = 'SM'
+
+
+    
+    # Get event data:
+    eventData = bannerData.split('<MGGenerationInfo>')[1].split('</MGGenerationInfo>')[0]
+    nEvents = eval(eventData.split('\n')[1].split(':')[1].strip())
+    xsec = eval(eventData.split('\n')[2].split(':')[1].strip())
+
+    fileInfo = {'model' : model, 'process' : proc, '(mSDM,mPsiT,mT,yDM)' : (mSDM,mPsiT,mT,yDM),
+               'xsec (pb)' : xsec, 'nevents' : nEvents}
+    
+    return fileInfo
+
