58038d5a3a0bae50d439393dcb285cb6ba5c61a7
viniciusrocca <vinicius_rocca@hotmail.com>
2025-12-09 20:53:53 -0300

    Trying to create a generalized bias using machine learning

--- auxFiles/mtt_exp_bias/qq2ttbar_gs4_ydm2/mtt_exp_bias_NLO.f
+++ auxFiles/mtt_exp_bias/qq2ttbar_gs4_ydm2/mtt_exp_bias_NLO.f
@@ -1,102 +1,116 @@
-
-      
-      subroutine bias_weight_function(p,ipdg,bias_wgt)
-c This is a user-defined function to which to bias the event generation.
-c A non-flat distribution will generate events with a certain weight
-c inversely proportinal to the bias_wgt. This is particularly useful to
-c generate more events (with smaller weight) in tails of distributions.
-c It computes the bias_wgt factor from the momenta and multiplies the
-c weight that goes into MINT (or vegas) with this factor.  Before
-c writing out the events (or making the plots), this factor is again
-c divided out. A value different from 1 makes that MINT (or vegas) does
-c not list the correct cross section, but the cross section can still be
-c computed from summing all the weights of the events (and dividing by
-c the number of events). Since the weights of the events are no longer
-c identical for all events, the statistical uncertainty on this total
-c cross section can be much larger than without including the bias.
+subroutine bias_weight_function(p,ipdg,bias_wgt)
+c     ==================================================================
 c
-c The 'bias_wgt' should be a IR-safe function of the momenta.
-c      
-c For this to be used, the 'event_norm' option in the run_card should be
-c set to
-c      'bias' = event_norm      
-c
+c     REQUIREMENTS:
+c     1. mod_bsm_pred.o must be linked during compilation.
+c     2. ../MODEL/input.inc must exist and contain mdl_mpsit/mdl_msdm.
+c     ==================================================================
+      use mod_bsm_pred, only: get_bsm_amplitude
       implicit none
+      
+c     --- MADGRAPH INCLUDES ---
       include 'nexternal.inc'
-      double precision bias_wgt,p(0:3,nexternal),H_T
-      integer ipdg(nexternal),i,j
+      include '../MODEL/input.inc' 
+
+c     --- ARGUMENTS ---
+      double precision bias_wgt
+      double precision p(0:3,nexternal)
+      integer ipdg(nexternal)
+
+c     --- LOCAL VARIABLES ---
+      integer i, j
       double precision pTot(0:3)
       double precision mtt
-      double precision polynomial_arg
-      double precision polynomial_offset
-
       
-c local variables defined in the run_card
-c
-c      double precision mtt_bias_target_mtt
-c      double precision mtt_bias_enhancement_power
+c     --- DYNAMIC BOUNDARY VARIABLES ---
+      double precision peak_loc, dip_loc
+      double precision b1, b2, b3, b4, b5
+      
+c     --- STITCHING & EVALUATION VARIABLES ---
+      double precision eps
+      parameter (eps = 1d-3) ! Small offset to check boundary jumps
+      double precision val_raw
+      double precision gap1, gap2, gap3, gap4, gap5
+      double precision total_stitch_offset
+      double precision unnormalized_val
+      
+c     --- NORMALIZATION VARIABLES ---
+      double precision, parameter :: mtt_norm = 1000.0d0
+      double precision val_at_norm, norm_stitch_offset
 
-      bias_wgt=1d0
+
+      bias_wgt = 1d0
+      
+      if (mdl_mpsit .lt. 1.0d0) return
+
+c     Calculate Invariant Mass of the Top Quark Pair 
       mtt = 0d0
       pTot = (/ 0d0, 0d0, 0d0, 0d0 /)
+      
       do i=1,nexternal
-          if (abs(ipdg(i)).eq.6) then
-          ! if (abs(idup(i,1,1)).eq.6) then
+         if (abs(ipdg(i)).eq.6) then
             do j =0,3
-              pTot(j) = pTot(j)+p(j,i)
+               pTot(j) = pTot(j)+p(j,i)
             enddo 
-          endif
-      enddo
-
-      mtt = dsqrt(pTot(0)**2 - pTot(1)**2 - pTot(2)**2 - pTot(3)**2)
-
-      if (mtt.gt.0.0d0) then
-         if (mtt .lt. 1051.0d0) then
-            polynomial_arg = - ( 5.1d-06 * mtt**2 - 10.0d-03 * mtt - 1.1d0 )
-c            polynomial_offset = - ( 5.1d-06 * 1000.0**2 - 10.0d-03 * 1000.0 - 1.1d0 )
-c            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-            bias_wgt = EXP(polynomial_arg)
-
-            
-         else if (mtt .ge. 1051.0d0 .and. mtt .lt. 1451.0d0) then
-            polynomial_arg = - ( - 3.0d-06 * mtt**2 - 1.7d-03 * mtt - 1.0 )
-c            polynomial_offset = - ( 5.1d-06 * 1000.0**2 - 10.0d-03 * 1000.0 - 1.1d0 )
-c            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-
-         else if (mtt .ge. 1451.0d0 .and. mtt .lt. 1701.0d0) then
-            polynomial_arg = - (  1.7d-04 * mtt**2 - 5.3d-01 * mtt + 4.0379d+02 )
-c            polynomial_offset = - ( 5.1d-06 * 1000.0**2 - 10.0d-03 * 1000.0 - 1.1d0 )
-c            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-            bias_wgt = EXP(polynomial_arg - polynomial_offset) 
-
-        else if (mtt .ge. 1701.0d0 .and. mtt .lt. 2701.0d0) then
-            polynomial_arg = - ( - 4.8d-07 * mtt**2 + 3.0d-04 * mtt - 9.6 )
-c            polynomial_offset = - ( 5.1d-06 * 1000.0**2 - 10.0d-03 * 1000.0 - 1.1 )
-c            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-         else
-            mtt = min(mtt,3200.0)
-            polynomial_arg = - (  2.2d-07 * mtt**2 - 3.9d-03 * mtt - 3.4 )
-c            polynomial_offset = - ( 5.1d-06 * 1000.0**2 - 10.0d-03 * 1000.0 - 1.1 )
-c            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-            bias_wgt = EXP(polynomial_arg - polynomial_offset)
-
          endif
+      enddo
+      
+
+
+      if (mtt .le. 0.0d0) return
+
+c     --- CMS CONSTRAINT: HARD CUTOFF AT 3500 GEV ---
+      if (mtt .gt. mtt_cutoff) then
+          bias_wgt = 1d0
+          return
       endif
 
-c How to enhance the tails is very process dependent. For example for
-c top quark production one could use:
-c      do i=1,nexternal
-c         if (ipdg(i).eq.6) then
-c            bias_wgt=sqrt(p(1,i)**2+p(2,i)**2)**3
-c         endif
-c      enddo
-c Or to use H_T^2 one does     
-c      H_T=0d0
-c      do i=3,nexternal
-c         H_T=H_T+sqrt(max(0d0,(p(0,i)+p(3,i))*(p(0,i)-p(3,i))))
-c      enddo
-c      bias_wgt=H_T**2
+c     Computing the boundaries
+      peak_loc = 2.05d0 * mdl_mpsit
+      dip_loc  = 2.6d0 * mdl_mpsit + 0.5d0 * mdl_msdm + 37.5d0
+
+      b1 = 0.9d0 * peak_loc
+      b2 = peak_loc + 50.0d0
+      b3 = dip_loc - 60.0d0
+      b4 = dip_loc + 140.0d0
+      b5 = dip_loc + 1900.0d0
+
+c    This will make the bias function smooth
+      
+      gap1 = get_bsm_amplitude(b1-eps, mdl_mpsit, mdl_msdm) - get_bsm_amplitude(b1+eps, mdl_mpsit, mdl_msdm)
+      gap2 = get_bsm_amplitude(b2-eps, mdl_mpsit, mdl_msdm) - get_bsm_amplitude(b2+eps, mdl_mpsit, mdl_msdm)
+      gap3 = get_bsm_amplitude(b3-eps, mdl_mpsit, mdl_msdm) - get_bsm_amplitude(b3+eps, mdl_mpsit, mdl_msdm)
+      gap4 = get_bsm_amplitude(b4-eps, mdl_mpsit, mdl_msdm) - get_bsm_amplitude(b4+eps, mdl_mpsit, mdl_msdm)
+      gap5 = get_bsm_amplitude(b5-eps, mdl_mpsit, mdl_msdm) - get_bsm_amplitude(b5+eps, mdl_mpsit, mdl_msdm)
+
+
+      
+      ! Get raw value from AI
+      val_raw = get_bsm_amplitude(mtt, mdl_mpsit, mdl_msdm)
+      
+      ! Add cumulative gaps depending on which region we are in
+      total_stitch_offset = 0d0
+      if (mtt .ge. b1) total_stitch_offset = total_stitch_offset + gap1
+      if (mtt .ge. b2) total_stitch_offset = total_stitch_offset + gap2
+      if (mtt .ge. b3) total_stitch_offset = total_stitch_offset + gap3
+      if (mtt .ge. b4) total_stitch_offset = total_stitch_offset + gap4
+      if (mtt .ge. b5) total_stitch_offset = total_stitch_offset + gap5
+      
+      unnormalized_val = val_raw + total_stitch_offset
+      
+c This will guarantee an initial supression and a numerical stability
+      
+      val_at_norm = get_bsm_amplitude(mtt_norm, mdl_mpsit, mdl_msdm)
+      norm_stitch_offset = 0d0
+      
+      if (mtt_norm .ge. b1) norm_stitch_offset = norm_stitch_offset + gap1
+      if (mtt_norm .ge. b2) norm_stitch_offset = norm_stitch_offset + gap2
+      if (mtt_norm .ge. b3) norm_stitch_offset = norm_stitch_offset + gap3
+      if (mtt_norm .ge. b4) norm_stitch_offset = norm_stitch_offset + gap4
+      if (mtt_norm .ge. b5) norm_stitch_offset = norm_stitch_offset + gap5
+      
+
+      bias_wgt = EXP( (val_at_norm + norm_stitch_offset) - unnormalized_val )
+
       return
       end
